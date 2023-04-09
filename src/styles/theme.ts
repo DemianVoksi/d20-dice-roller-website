b741537dd63ccec7f958ad4840c8767355e9b558
Demian Voksi <demian.voksi@gmail.com>
2023-04-09 19:24:38 +0200

    initial commit

---
+++ src/styles/theme.ts
@@ -0,0 +1,15 @@
+import { extendTheme } from '@chakra-ui/react';
+
+export const myNewTheme = extendTheme({
+	colors: {
+		black: '#030303',
+		white: '#ffffff',
+		grayDark: '#2a2727',
+		grayLight: '#12181c',
+		redLight: '#e40712',
+		redDark: '#930C10',
+		creme: '#fffebd',
+		cremeLight: '#e8e5d9',
+		cremeDark: '#f7ce65'
+	}
+});
